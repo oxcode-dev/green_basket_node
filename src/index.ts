1e33089eedaaf97e8e3f52ca601bc0fb25e880be
Oxcode <mrexcelsam1@gmail.com>
2026-04-04 16:11:10 +0100

    modify

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express, { type Application } from "express";
 import dotenv from 'dotenv'
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
+
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(cookieParser());
 
 const PORT: number | string = 2000;
 
