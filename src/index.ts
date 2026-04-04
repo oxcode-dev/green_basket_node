a327505bb774c9a7a01d0ac53d8cc924d93c2ac0
Oxcode <mrexcelsam1@gmail.com>
2026-04-04 16:00:37 +0100

    modify

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ import express, { type Application } from "express";
 const app: Application = express();
 
 app.use(express.json());
+
+const PORT: number | string = 2000;
+
+app.listen(PORT, () =>
+  console.log(
+    `🟢 Server running in development mode on port ${PORT}`
+  )
+);
