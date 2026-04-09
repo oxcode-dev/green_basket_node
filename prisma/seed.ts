f71f7cd33c60310da11fc5ebb4450745ec00db1a
Oxcode <mrexcelsam1@gmail.com>
2026-04-09 18:01:01 +0100

    modify

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../src/generated/prisma/client"
+import { faker } from '@faker-js/faker';
 
 
 const connectionString = `${process.env.DATABASE_URL}`;
@@ -10,44 +11,77 @@ const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  const alice = await prisma.users.upsert({
-    where: { email: "alice@prisma.io" },
-    update: {},
-    create: {
-      email: "alice@prisma.io",
-      name: "Alice",
-      posts: {
-        create: {
-          title: "Check out Prisma with Next.js",
-          content: "https://www.prisma.io/nextjs",
-          published: true,
-        },
-      },
-    },
-  });
-  const bob = await prisma.user.upsert({
-    where: { email: "bob@prisma.io" },
-    update: {},
-    create: {
-      email: "bob@prisma.io",
-      name: "Bob",
-      posts: {
-        create: [
-          {
-            title: "Follow Prisma on Twitter",
-            content: "https://twitter.com/prisma",
-            published: true,
-          },
-          {
-            title: "Follow Nexus on Twitter",
-            content: "https://twitter.com/nexusgql",
-            published: true,
-          },
-        ],
-      },
-    },
-  });
-  console.log({ alice, bob });
+    // const alice = await prisma.users.upsert({
+    //     where: { email: "alice@prisma.io" },
+    //     update: {},
+    //     create: {
+    //         email: "alice@prisma.io",
+    //         first_name: "Alice",
+    //         last_name: "Alice",
+    //         // posts: {
+    //         //     create: {
+    //         //         title: "Check out Prisma with Next.js",
+    //         //         content: "https://www.prisma.io/nextjs",
+    //         //         published: true,
+    //         //     },
+    //         // },
+    //     },
+    // });
+
+    // console.log({ alice });
+
+//       category_id String? @db.Uuid
+//   title String
+//   slug String
+//   summary String?
+//   description String?
+//   price Decimal @db.Decimal(10,2) @default(0.00)
+//   stock Int @default(0)
+//   is_active Boolean @default(true)
+//   image String?
+//   created_at DateTime @db.Timestamp(6) @default(now())
+//   updated_at DateTime @db.Timestamp(6) @default(now())
+
+// model categories {
+//   id String @id @default(dbgenerated("gen_random_uuid()")) @db.Uuid
+//   name String
+//   slug String
+//   description String?
+//   created_at DateTime @db.Timestamp(6) @default(now())
+//   updated_at DateTime @db.Timestamp(6) @default(now()) @updatedAt()
+
+//   products products[]
+// }
+
+    const categories = await prisma.categories.createMany({
+        data: [
+            {
+                name: faker.commerce.department(),
+                slug: faker.helpers.slugify(faker.commerce.department()).toLowerCase(),
+                description: faker.commerce.productDescription()
+            },
+            {
+                name: faker.commerce.department(),
+                slug: faker.helpers.slugify(faker.commerce.department()).toLowerCase(),
+                description: faker.commerce.productDescription()
+            },
+            {
+                name: faker.commerce.department(),
+                slug: faker.helpers.slugify(faker.commerce.department()).toLowerCase(),
+                description: faker.commerce.productDescription()
+            },
+            {
+                name: faker.commerce.department(),
+                slug: faker.helpers.slugify(faker.commerce.department()).toLowerCase(),
+                description: faker.commerce.productDescription()
+            },
+            {
+                name: faker.commerce.department(),
+                slug: faker.helpers.slugify(faker.commerce.department()).toLowerCase(),
+                description: faker.commerce.productDescription()
+            },
+        ]
+    })
 }
 
 main()
