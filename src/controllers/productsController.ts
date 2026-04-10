2339a97fda7d06eed549edd85570caac31483e33
Oxcode <mrexcelsam1@gmail.com>
2026-04-10 02:33:28 +0100

    modify

---
+++ src/controllers/productsController.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { prisma } from '../lib/prisma.ts';
+
+export const getProducts = async(req: express.Request, res: express.Response) => {
+    try {
+        const products = await prisma.products.findMany();
+
+        return res.status(200).json({
+            message: "Products retrieved successfully!!!",
+            products
+        })
+        
+    } catch (error) {
+        return res.status(500).json({ message: `Server error: ${error}` });
+    }
+}
+
+export const getCategory = async(req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        
+        const product = await prisma.products.findFirst({
+            where: { 
+                id: Array.isArray(id) ? id[0] : id
+            },
+            include: { category: true }
+        });
+
+        return res.status(200).json({
+            message: "Category retrieved successfully!!!",
+            product
+        })
+        
+    } catch (error) {
+        return res.status(500).json({ message: `Server error: ${error}` });
+    }
+}
