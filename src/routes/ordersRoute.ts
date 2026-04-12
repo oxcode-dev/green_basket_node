a901a1d9bf23dca5af5de8ae42dd01afeac68d46
Oxcode <mrexcelsam1@gmail.com>
2026-04-12 17:34:42 +0100

    modify

--- src/routes/ordersRoute.ts
+++ src/routes/ordersRoute.ts
@@ -1,18 +1,16 @@
 import express from 'express';
-import { 
-    getUserDetails, changePassword, updateUserDetails, deleteProfile
-} from '../controllers/profileController.ts';
+
 import { auth } from '../middlewares/authMiddleware.ts';
 import { validateInputData } from '../middlewares/validate.ts';
 import { changePasswordSchema, userDetailsSchema } from '../validationSchemas/profileSchema.ts';
+import { getUserOrder, getUserOrders } from '../controllers/ordersController.ts';
 
 const router = express.Router();
 
-router.route('/profile')
-    .get(auth, getUserDetails as any)
-    .put(auth, validateInputData(userDetailsSchema), updateUserDetails as any);
-    
-router.post('/change-password', auth, validateInputData(changePasswordSchema), changePassword as any);
-router.delete('/delete-account', auth, deleteProfile as any);
+router.route('/')
+    .get(auth, getUserOrders as any)
 
-export { router as profileRouter };
+router.route('/:id')
+    .get(auth, getUserOrder as any)
+
+export { router as ordersRouter };
