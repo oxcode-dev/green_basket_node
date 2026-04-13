2b5f867fe2c84819a363a58f1740f5170e372bd5
Oxcode <mrexcelsam1@gmail.com>
2026-04-13 01:13:36 +0100

    modify

---
+++ src/routes/addressesRoute.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { deleteUserAddress, getUserAddress, getUserAddresses, storeUserAddress, updateUserAddress } from '../controllers/addressesController.ts';
+import { validateInputData } from '../middlewares/validate.ts';
+import { userAddressSchema } from '../validationSchemas/profileSchema.ts';
+import { auth } from '../middlewares/authMiddleware.ts';
+
+const router = express.Router();
+
+router.route('/')
+    .get(auth, getUserAddresses)
+    .post(auth, validateInputData(userAddressSchema), storeUserAddress);
+
+router.route('/:id')
+    .get(auth, getUserAddress)
+    .put(auth, validateInputData(userAddressSchema), updateUserAddress)
+    .delete(auth, deleteUserAddress);
+
+export { router as addressesRoute };
