b5e6b5c2cd54816b36ab3b28346ae7b5f625e52c
Oxcode <mrexcelsam1@gmail.com>
2026-04-14 09:39:33 +0100

    modify

---
+++ src/routes/reviewsRoute.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { auth } from '../middlewares/authMiddleware.ts';
+import { validateInputData } from '../middlewares/validate.ts';
+import { productReviewSchema } from '../validationSchemas/profileSchema.ts';
+import { getReviews, getReview, deleteReview, storeReview } from '../controllers/reviewsController.ts';
+
+const router = express.Router();
+
+router.get('/', auth, getReviews)
+router.post('/', auth, validateInputData(productReviewSchema), storeReview)
+router.get('/:id', auth, getReview)
+router.delete('/:id', auth, deleteReview)
+
+export { router as reviewsRoute };
