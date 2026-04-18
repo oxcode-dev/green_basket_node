a2e5919060c5e7166f45a4d7680d426df2766030
Oxcode <mrexcelsam1@gmail.com>
2026-04-19 00:12:55 +0100

    modify

--- src/validationSchemas/uploadSchema.ts
+++ src/validationSchemas/uploadSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/svg+xml", "image/gif",];
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/svg+xml", "image/gif"];
 
 export const imageSchema = z
     .instanceof(File)
@@ -16,25 +16,6 @@ export const documentSchema = z
     .instanceof(File)
     .refine(
         (file) =>
-        [
-            "application/pdf",
-            "application/vnd.ms-excel",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        ].includes(file.type),
+        .includes(file.type),
         { message: "Invalid document file type" }
     );
-
-// Image Schema
-export const IMAGE_SCHEMA = z
-  .instanceof(File)
-  .refine(
-    (file) =>
-      [
-        "image/png",
-        "image/jpeg",
-        "image/jpg",
-        "image/svg+xml",
-        "image/gif",
-      ].includes(file.type),
-    { message: "Invalid image file type" }
-  );
