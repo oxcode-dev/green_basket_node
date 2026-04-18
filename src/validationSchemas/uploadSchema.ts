57e9e08078d7cd8ea5654632c46b5b2e8b8c8738
Oxcode <mrexcelsam1@gmail.com>
2026-04-19 00:08:04 +0100

    modify

---
+++ src/validationSchemas/uploadSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+
+export const fileSchema = z
+  .instanceof(File)
+  .refine((file) => file.size <= MAX_FILE_SIZE, `Max file size is 5MB.`)
+  .refine(
+    (file) => ACCEPTED_IMAGE_TYPES.includes(file.type),
+    "Only .jpg, .jpeg and .png formats are supported."
+  );
