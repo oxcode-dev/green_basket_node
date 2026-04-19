2b1ad0b55a920c8201bddd0fe6bc565f135a6ed9
Oxcode <mrexcelsam1@gmail.com>
2026-04-19 21:14:06 +0100

    modify

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,6 @@ app.get('/test', async(req, res) => {
     return res.status(200).json({users, categories, products});
 });
 
-// app.post('/api/upload', upload.single('file'), (req: any, res: express.Response) => {
 app.post('/api/upload', localUpload, (req: any, res: express.Response) => {
     if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded' });
