f6e1708758d9820e6eb6bad99bb165e2dc90e2f5
Oxcode <mrexcelsam1@gmail.com>
2026-04-20 23:13:31 +0100

    modify

--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -66,3 +66,69 @@ export const getUserOrder = async (req: any, res: express.Response) => {
         return res.status(500).json({ message: `server error: ${error}`})
     }
 }
+
+const checkout = async (req: any, res: express.Response) => {
+    try {
+        const axios = require("axios");
+        const Order = require("../models/Order");
+        const Cart = require("../models/Cart");
+
+
+        const userId = req.user.id;
+
+        // 1. Get user cart
+        const cart = await Cart.findOne({ user: userId }).populate("items.product");
+
+        if (!cart || cart.items.length === 0) {
+            return res.status(400).json({ message: "Cart is empty" });
+        }
+
+        // 2. Calculate total
+        const totalAmount = 0
+        // const totalAmount = cart.items.reduce((acc, item) => {
+        //     return acc + item.product.price * item.quantity;
+        // }, 0);
+
+        // 3. Create order (PENDING)
+        const order = await Order.create({
+            user: userId,
+            // items: cart.items.map(item => ({
+            //     product: item.product._id,
+            //     quantity: item.quantity,
+            //     price: item.product.price
+            // })),
+            totalAmount
+        });
+
+        // 4. Initialize Paystack payment
+        const paystackRes = await axios.post(
+            "https://api.paystack.co/transaction/initialize",
+            {
+                email: req.user.email,
+                amount: totalAmount * 100,
+                callback_url: `${process.env.BASE_URL}/api/payment/verify`,
+                metadata: {
+                orderId: order._id
+                }
+            },
+            {
+                headers: {
+                Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
+                }
+            }
+        );
+
+        // 5. Save reference
+        order.paymentReference = paystackRes.data.data.reference;
+        await order.save();
+
+        // 6. Return payment link
+        res.json({
+            paymentUrl: paystackRes.data.data.authorization_url,
+            orderId: order._id
+        });
+
+    } catch (err: any) {
+        res.status(500).json({ error: err.message });
+    }
+}
