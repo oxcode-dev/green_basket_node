0e83e96deea720fd1f1cdd25eab55b7150067b67
Oxcode <mrexcelsam1@gmail.com>
2026-04-22 18:36:35 +0100

    modify

--- src/routes/wishlistsRoute.ts
+++ src/routes/wishlistsRoute.ts
@@ -2,12 +2,13 @@ import express from 'express';
 
 import { auth } from '../middlewares/authMiddleware.ts';
 import { deleteWishlist, getUserWishlist, getUserWishlists, storeWishlist } from '../controllers/wishlistsController.ts';
+import { handleCustomer } from '../middlewares/handleUserRole.ts';
 
 const router = express.Router();
 
 router.get('/', auth, getUserWishlists)
-router.post('/:product_id', auth, storeWishlist)
+router.post('/:product_id', auth, handleCustomer, storeWishlist)
 router.get('/:id', auth, getUserWishlist)
-router.delete('/:id', auth, deleteWishlist)
+router.delete('/:id', auth, handleCustomer, deleteWishlist)
 
 export { router as wishlistsRoute };
