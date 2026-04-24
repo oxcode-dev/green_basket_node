f74d01dd2b0259f6d5fd04297d06ecb096eca412
Oxcode <mrexcelsam1@gmail.com>
2026-04-24 17:31:38 +0100

    modify

--- src/routes/ordersRoute.ts
+++ src/routes/ordersRoute.ts
@@ -4,13 +4,14 @@ import { auth } from '../middlewares/authMiddleware.ts';
 import { validateInputData } from '../middlewares/validate.ts';
 import { changePasswordSchema, userDetailsSchema } from '../validations/profileValidation.ts';
 import { getUserOrder, getUserOrders } from '../controllers/ordersController.ts';
+import { handleCustomer } from '../middlewares/handleUserRole.ts';
 
 const router = express.Router();
 
 router.route('/')
-    .get(auth, getUserOrders as any)
+    .get(auth, handleCustomer, getUserOrders)
 
 router.route('/:id')
-    .get(auth, getUserOrder as any)
+    .get(auth, handleCustomer, getUserOrder)
 
 export { router as ordersRouter };
