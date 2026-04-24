322df9ea81e50cc35916d7bf0384d1cbccb71b3f
Oxcode <mrexcelsam1@gmail.com>
2026-04-24 01:37:59 +0100

    modify

--- src/routes/adminRoute.ts
+++ src/routes/adminRoute.ts
@@ -5,17 +5,24 @@ import { userLogin, userLogout } from '../controllers/authController.ts';
 import { loginSchema } from '../validations/authValidation.ts';
 import { handleAdmin } from '../middlewares/handleUserRole.ts';
 import { createCategory, deleteCategory, updateCategory } from '../controllers/categoriesController.ts';
+import { createProduct, deleteProduct, updateProduct } from '../controllers/productsController.ts';
 
 const router = express.Router();
 
 router.post('/login', validateInputData(loginSchema), userLogin);
 router.delete('/logout', auth, userLogout);
 
+// Category Routes
 router.post('/category', auth, handleAdmin, createCategory);
-
 router.route('/category/:id')
     .put(auth, handleAdmin, updateCategory)
     .delete(auth, handleAdmin, deleteCategory);
+
+// Product Routes
+router.post('/category', auth, handleAdmin, createProduct);
+router.route('/category/:id')
+    .put(auth, handleAdmin, updateProduct)
+    .delete(auth, handleAdmin, deleteProduct);
 
 
 
