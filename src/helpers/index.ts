4a374e490b6a9ec1f235e9f061edf3251cb28bb9
Oxcode <mrexcelsam1@gmail.com>
2026-04-24 10:10:03 +0100

    modify

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -20,3 +20,13 @@ export function generatePin($digits = 4) {
 
     return pin;
 }
+
+export function slugify(text: string): string {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')     // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-');    // Replace multiple - with single -
+}
