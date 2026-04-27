ef7d2679eb5508cbf875ff9456fc6c9db02a4ebb
Oxcode <mrexcelsam1@gmail.com>
2026-04-28 00:35:58 +0100

    modify

---
+++ src/middlewares/handleCart.ts
@@ -0,0 +1,52 @@
+import { config } from "dotenv";
+import express from "express";
+import jwt from 'jsonwebtoken'
+import { prisma } from "../lib/prisma.ts";
+// import { AuthUserType, DataStoredInToken } from "../types/index.ts";
+
+config();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export interface DataStoredInToken {
+  id: string;
+  email: string;
+}
+
+const checkAuthForCart = async (req: any, res: express.Response, next: express.NextFunction)  => {
+
+    try {
+        const authHeader = req.headers['authorization'];
+
+        if(!authHeader || !authHeader.startsWith('Bearer ')) {
+            return next()
+            // return res.status(401).json({ message: 'Kindly login to access this resource' });
+        }
+
+        const token : string = authHeader.split(' ')[1] || '';
+        
+        const decoded = jwt.verify(
+            token,
+            JWT_SECRET
+        ) as DataStoredInToken;
+
+        const user = await prisma.users.findFirst({
+            where: {id: decoded?.id},
+        })
+
+        if(user) {
+            req.user = {
+                id: user?.id,
+                email: user?.email,
+                role: user?.role,
+            }
+        }
+
+        next();
+    } catch (error) {
+        // console.error("Token verification error: ", error || 'Server error');
+        res.status(401).json({ message: "User Logged out" });
+    }
+}
+
+export { checkAuthForCart };
--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import { addToCart, clearCart, getCart, removeCartItem, updateCartItem } from '../controllers/cartController.ts';
+import { checkAuthForCart } from '../middlewares/handleCart.ts';
 
 
 const router = express.Router();
 
-router.post('/add', addToCart);
+router.post('/add', checkAuthForCart, addToCart);
 
-router.get('/get', getCart);
+router.get('/get', checkAuthForCart, getCart);
 
-router.put('/update/:productId', updateCartItem);
+router.put('/update/:productId', checkAuthForCart, updateCartItem);
 
-router.delete('/remove/:productId', removeCartItem)
+router.delete('/remove/:productId', checkAuthForCart, removeCartItem)
 
 router.delete('/clear', clearCart)
 
