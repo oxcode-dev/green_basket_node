5853bec639b3feb3e25ad02972bba90ad439fb51
Oxcode <mrexcelsam1@gmail.com>
2026-04-29 14:11:37 +0100

    modify

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import type { PaginationType } from '../types/index.ts';
 import { prisma } from '../lib/prisma.ts';
-import { countUsersByRole, fetchUser, fetchUsersByRoleWithPagination } from '../services/usersServices.ts';
+import { countUsersByRole, fetchUser, fetchUsersByRoleWithPagination, storeUser } from '../services/usersServices.ts';
+import { get } from 'node:http';
+import { generatePassword } from '../helpers/index.ts';
 
 export const getAdminUsers = async (req: express.Request | PaginationType, res: express.Response) => {
     try {
@@ -59,6 +61,31 @@ export const getUser = async (req: express.Request, res: express.Response) => {
 
         return res.status(200).json({
             message: "User fetched successfully!!!",
+            status: 'success',
+            user,
+        });
+        
+    } catch (error) {
+        return res.status(500).json({ message: `server error: ${error}`})
+    }
+}
+
+export const createUserByAdmin = async (req: express.Request, res: express.Response) => {
+    try {
+        const { first_name, email, last_name, role, phone,  } = req.body;                
+
+        // const user = await storeUser(String(id));
+        const user = await storeUser({
+            email,
+            first_name,
+            last_name,
+            role: role as 'ADMIN' | 'CUSTOMER',
+            phone,
+            password: generatePassword(8),
+        });
+
+        return res.status(200).json({
+            message: "User created successfully!!!",
             status: 'success',
             user,
         });
