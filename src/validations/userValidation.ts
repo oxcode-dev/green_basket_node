3a0cb04b5c9cff423664d96980a695b73a41b5a4
Oxcode <mrexcelsam1@gmail.com>
2026-04-30 00:49:48 +0100

    modify

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -1,12 +1,9 @@
 import z from "zod";
 
-export const productValidation = z.object({
-    title: z.string().min(2, { message: "Name must be at least 2 characters long" }),
-    description: z.string().optional(),
-    summary: z.string().min(2, { message: "Name must be at least 2 characters long" }),
-    category_id: z.string().min(2, { message: "Name must be at least 2 characters long" }),
-    price: z.number().positive({ message: "Price must be a positive number" }),
-    stock: z.number().int().nonnegative({ message: "Stock must be a non-negative integer" }),
-    is_active: z.boolean(),
-    // image: z.string().url({ message: "Image must be a valid URL" }),
-})
+export const userDetailsValidation = z.object({
+    first_name: z.string().min(2, { message: "First name must be at least 2 characters long" }),
+    last_name: z.string().min(2, { message: "Last name must be at least 2 characters long" }),
+    bio: z.string().optional(),
+    phone: z.string().optional(),
+    email: z.string().email({ message: "Invalid email address" }).trim().toLowerCase(),
+}).strict();
