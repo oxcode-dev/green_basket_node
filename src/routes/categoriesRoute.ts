57353b74228d911821be527cf774be71b6692279
Oxcode <mrexcelsam1@gmail.com>
2026-05-06 16:39:19 +0100

    modify

--- src/routes/categoriesRoute.ts
+++ src/routes/categoriesRoute.ts
@@ -3,7 +3,43 @@ import { getCategories, getCategory } from '../controllers/categoriesController.
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Categories
+ *   description: Category List and Details
+ */
+
+/**
+ * @swagger
+ * /api/categories:
+ *   get:
+ *     summary: Get all categories with pagination
+ *     tags: [Categories]
+ *     responses:
+ *       200:
+ *         description: List of categories and pagination metadata
+ */
 router.get('/', getCategories);
+/**
+ * @swagger
+ * /api/categories/{id}:
+ *   get:
+ *     summary: Get a single category by ID
+ *     tags: [Categories]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category ID
+ *     responses:
+ *       200:
+ *         description: category details
+ *       404:
+ *         description: Category not found
+ */
 router.get('/:id', getCategory);
 
 export { router as categoriesRoute };
