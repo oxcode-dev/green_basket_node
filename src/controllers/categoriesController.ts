4aa9e8a073fd0c67e95e96eab308754d5e08b765
Oxcode <mrexcelsam1@gmail.com>
2026-05-08 13:17:03 +0100

    modify

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -2,79 +2,56 @@ import express, {type Request} from 'express';
 import { destroyCategory, fetchCategories, fetchCategory, storeCategory, updateCategory } from '../services/categoryServices.ts';
 
 export const getCategories = async(req: express.Request, res: express.Response) => {
-    // try {
-        const categories = await fetchCategories();
+    const categories = await fetchCategories();
 
-        return res.status(200).json({
-            message: "Categories retrieved successfully!!!",
-            categories
-        })
-        
-    // } catch (error) {
-    //     return res.status(500).json({ message: `Server error: ${error}` });
-    // }
+    return res.status(200).json({
+        message: "Categories retrieved successfully!!!",
+        categories
+    })
 }
 
 export const getCategory = async(req: Request, res: express.Response) => {
-    try {
-        const { id } = req.params as {id: string};
-        
-        const category = await fetchCategory(id);
+    const { id } = req.params as {id: string};
+    
+    const category = await fetchCategory(id);
 
-        return res.status(200).json({
-            message: "Category retrieved successfully!!!",
-            category
-        })
-        
-    } catch (error) {
-        return res.status(500).json({ message: `Server error: ${error}` });
-    }
+    return res.status(200).json({
+        message: "Category retrieved successfully!!!",
+        category
+    })
 }
 
 
 export const createCategory = async (req: express.Request, res: express.Response) => {
-    try {
-        const { name, description } = req.body;
+    const { name, description } = req.body;
 
-        const category = await storeCategory(name, description);
+    const category = await storeCategory(name, description);
 
-        return res.status(201).json({
-            message: "Category created successfully!!!",
-            category
-        })
-        
-    } catch (error) {
-        return res.status(500).json({ message: `Server error: ${error}` });
-    }
+    return res.status(201).json({
+        message: "Category created successfully!!!",
+        category
+    })
 }
 
 export const editCategory = async (req: express.Request, res: express.Response) => {
-    try {
-        const { name, description } = req.body;
+    const { name, description } = req.body;
 
-        const { id } = req.params as { id: string }; 
+    const { id } = req.params as { id: string }; 
 
-        const category = await updateCategory(id, name, description);
+    const category = await updateCategory(id, name, description);
 
-        return res.status(201).json({
-            message: "Category updated successfully!!!",
-            category
-        })
-    } catch (error) {
-        return res.status(500).json({ message: `Server error: ${error}` });
-    }
+    return res.status(201).json({
+        message: "Category updated successfully!!!",
+        category
+    })
 }
 
 export const deleteCategory = async (req: express.Request, res: express.Response) => {
-    try {
-        const { id } = req.params as { id: string }; 
+    const { id } = req.params as { id: string }; 
 
-        await destroyCategory(id);
+    await destroyCategory(id);
 
-        return res.status(201).json({
-            message: "Category deleted successfully!!!",
-        })
-    } catch (error) {
-        return res.status(500).json({ message: `Server error: ${error}` });
-    }
+    return res.status(201).json({
+        message: "Category deleted successfully!!!",
+    })
 }
--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -1,83 +1,71 @@
 import express from 'express';
 import type { PaginationType, RequestWithUser } from '../types/index.ts';
-import { countAllCustomerOrders, countAllOrders, fetchCustomerOrdersWithPagination, fetchOrder, fetchOrdersWithPagination } from '../services/OrderServices.ts';
+import { countAllCustomerOrders, countAllOrders, fetchCustomerOrdersWithPagination, fetchOrder, fetchOrdersWithPagination } from '../services/orderServices.ts';
 
 
 export const getUserOrders = async (req: RequestWithUser & PaginationType, res: express.Response) => {
-    try {
-        const auth = req.user
+    const auth = req.user
 
-        const { page, limit, skip } = req as PaginationType;
-        
-        const totalCount = await countAllCustomerOrders(String(auth?.id));
+    const { page, limit, skip } = req as PaginationType;
+    
+    const totalCount = await countAllCustomerOrders(String(auth?.id));
 
-        const orders = await fetchCustomerOrdersWithPagination(String(auth?.id), skip, limit);
+    const orders = await fetchCustomerOrdersWithPagination(String(auth?.id), skip, limit);
 
-        let data = {
-            status: "success",
-            message: "Orders fetched successfully",
-            orders,
-            metadata: {
-                page: page,
-                limit: limit,
-                totalCount,
-                totalPages: Math.ceil(totalCount / limit),
-            }
+    let data = {
+        status: "success",
+        message: "Orders fetched successfully",
+        orders,
+        metadata: {
+            page: page,
+            limit: limit,
+            totalCount,
+            totalPages: Math.ceil(totalCount / limit),
         }
-
-        res.status(200).json(data);
-    } catch (error) {
-        return res.status(500).json({ message: `server error: ${error}`})
     }
+
+    res.status(200).json(data);
+   
 }
 
 export const getOrder = async (req: RequestWithUser, res: express.Response) => {
-    try {
-        const auth = req.user
+    const auth = req.user
 
-        const id = String(req?.params?.id || '')
-        
-        const order = await fetchOrder(id);
+    const id = String(req?.params?.id || '')
+    
+    const order = await fetchOrder(id);
 
-        if (!order || order.user_id !== auth?.id) {
-            return res.status(404).json({ error: 'Order not found' })
-        }
-
-        let data = {
-            status: "success",
-            message: "Orders fetched successfully",
-            order,
-        }
-
-        res.status(200).json(data);
-        
-    } catch (error) {
-        return res.status(500).json({ message: `server error: ${error}`})
+    if (!order || order.user_id !== auth?.id) {
+        return res.status(404).json({ error: 'Order not found' })
     }
+
+    let data = {
+        status: "success",
+        message: "Orders fetched successfully",
+        order,
+    }
+
+    res.status(200).json(data);
 }
 
 export const getAllOrders = async (req: express.Request & PaginationType, res: express.Response) => {
-    try {
-        const { page, limit, skip } = req as PaginationType;
-        
-        const totalCount = await countAllOrders();
+    const { page, limit, skip } = req as PaginationType;
+    
+    const totalCount = await countAllOrders();
 
-        const orders = await fetchOrdersWithPagination(skip, limit);
+    const orders = await fetchOrdersWithPagination(skip, limit);
 
-        let data = {
-            status: "success",
-            message: "Orders fetched successfully",
-            orders,
-            metadata: {
-                page: page,
-                limit: limit,
-                totalCount,
-                totalPages: Math.ceil(totalCount / limit),
-            }
+    let data = {
+        status: "success",
+        message: "Orders fetched successfully",
+        orders,
+        metadata: {
+            page: page,
+            limit: limit,
+            totalCount,
+            totalPages: Math.ceil(totalCount / limit),
         }
-
-        res.status(200).json(data);
-    } catch (error) {
-        return res.status(500).json({ message: `server error: ${error}`})
     }
+
+    res.status(200).json(data);
 }
