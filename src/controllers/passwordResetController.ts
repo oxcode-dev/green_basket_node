634b1acd8c2028e3e734f6ca2286180ee3eb8aa0
Oxcode <mrexcelsam1@gmail.com>
2026-05-08 13:31:18 +0100

    modify

--- src/controllers/passwordResetController.ts
+++ src/controllers/passwordResetController.ts
@@ -9,82 +9,75 @@ const EMAIL_SMTP_USERNAME = process.env.EMAIL_SMTP_USERNAME as string;
 const CLIENT_URL = process.env.CLIENT_URL as string
 
 export const forgotPassword = async (req: express.Request, res: express.Response) => {
-    try {
-        const { email } = req.body as { email: string };
+    const { email } = req.body as { email: string };
 
-        const user = await fetchUserByEmail(email);
+    const user = await fetchUserByEmail(email);
 
-        if(!user) {
-            return res.status(400).json({ message: 'User not found!' });
-        }
-
-        await destroyOtpCodeByEmail(email)
-
-        const otpCode = await storeOtpCode({
-            code: generatePin(4),
-            email: user.email,
-            expires_at: new Date(Date.now() + 15 * 60 * 1000) // OTP expires in 15 minutes
-        })
-
-        await sendMail(
-            EMAIL_SMTP_USERNAME,
-            user.email,
-            // 'mrexcelsam1@gmail.com',
-            "Password Reset OTP",
-            `<p>Your account with email ${user.email} has been requested for password reset.</p>
-            <p>Your OTP is: <b>${otpCode.code}</b></p>
-            <p>Thank you for using our application!</p>
-            <p><a href="${CLIENT_URL}/reset-password">Click here to reset your password</a></p>`
-        );
-
-         let data = {
-            status: "success",
-            message: "Password reset OTP sent successfully! Please check your email.",
-        }
-
-        return res.status(201).send(data);
-    } catch (error) {
-        return res.status(500).json({ message: 'Server error' });
+    if(!user) {
+        return res.status(400).json({ message: 'User not found!' });
     }
+
+    await destroyOtpCodeByEmail(email)
+
+    const otpCode = await storeOtpCode({
+        code: generatePin(4),
+        email: user.email,
+        expires_at: new Date(Date.now() + 15 * 60 * 1000) // OTP expires in 15 minutes
+    })
+
+    await sendMail(
+        EMAIL_SMTP_USERNAME,
+        user.email,
+        // 'mrexcelsam1@gmail.com',
+        "Password Reset OTP",
+        `<p>Your account with email ${user.email} has been requested for password reset.</p>
+        <p>Your OTP is: <b>${otpCode.code}</b></p>
+        <p>Thank you for using our application!</p>
+        <p><a href="${CLIENT_URL}/reset-password">Click here to reset your password</a></p>`
+    );
+
+        let data = {
+        status: "success",
+        message: "Password reset OTP sent successfully! Please check your email.",
+    }
+
+    return res.status(201).send(data);
+   
 }
 
 export const resetPassword = async (req: express.Request, res: express.Response) => {
-    try {
-        const { email, otp, new_password } = req.body;
+    const { email, otp, new_password } = req.body;
 
-        const user = await fetchUserByEmail(email);
+    const user = await fetchUserByEmail(email);
 
-        if(!user) {
-            return res.status(400).json({ message: 'User not found' });
-        }
-
-        if(!otp || !new_password || !email) {
-            return res.status(400).json({ message: "Required fields are missing!" });
-        }
-
-        const otpCode = await fetchOtpCodeByEmail(email);
-
-        if(!otpCode) {
-            return res.status(400).json({ message: "Invalid OTP" });
-        }
-
-        if(otpCode && otpCode.expires_at && otpCode.expires_at < new Date()) {
-            return res.status(400).json({ message: "OTP has expired" });
-        }
-        
-        const hashedPassword = await bcrypt.hash(new_password, 12);
-
-        await updateUserPassword(user.id, { password: hashedPassword });
-
-        await destroyOtpCodeByEmail(user.email);
-
-        let data = {
-            status: "success",
-            message: "Password reset successfully! Please login with your new password.",
-        }
-
-        return res.status(201).send(data);
-    } catch (error) {
-        return res.status(500).json({ message: 'Server error' });
+    if(!user) {
+        return res.status(400).json({ message: 'User not found' });
     }
+
+    if(!otp || !new_password || !email) {
+        return res.status(400).json({ message: "Required fields are missing!" });
+    }
+
+    const otpCode = await fetchOtpCodeByEmail(email);
+
+    if(!otpCode) {
+        return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    if(otpCode && otpCode.expires_at && otpCode.expires_at < new Date()) {
+        return res.status(400).json({ message: "OTP has expired" });
+    }
+    
+    const hashedPassword = await bcrypt.hash(new_password, 12);
+
+    await updateUserPassword(user.id, { password: hashedPassword });
+
+    await destroyOtpCodeByEmail(user.email);
+
+    let data = {
+        status: "success",
+        message: "Password reset successfully! Please login with your new password.",
+    }
+
+    return res.status(201).send(data);
 }
--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { deleteCart, fetchCart } from '../services/cartServices.ts';
 import { getCartKey } from '../utils/index.ts';
 import axios from 'axios';
-import { fetchOrderByPaymentReference, storeOrder, storeOrderItem, updateOrder } from '../services/OrderServices.ts';
+import { fetchOrderByPaymentReference, storeOrder, storeOrderItem, updateOrder } from '../services/orderServices.ts';
 import crypto from "crypto"
 import dotenv from 'dotenv'
 import type { RequestWithUser } from '../types/index.ts';
@@ -10,109 +10,99 @@ import type { RequestWithUser } from '../types/index.ts';
 dotenv.config();
 
 export const checkout = async (req: RequestWithUser, res: express.Response) => {
-    try {
-        const key = getCartKey(req)
+    const key = getCartKey(req)
 
-        const userId = req?.user?.id || '';
+    const userId = req?.user?.id || '';
 
-        const { address_id  } = req.body
+    const { address_id  } = req.body
 
-        const cart = await fetchCart(key)
+    const cart = await fetchCart(key)
 
-        if (!cart || cart.items.length === 0) {
-            return res.status(400).json({ message: "Cart is empty" });
-        }
-
-        const totalAmount = cart?.total || 0
-        // const totalAmount = cart.items.reduce((acc, item) => {
-        //     return acc + item.product.price * item.quantity;
-        // }, 0);
-
-        const order = await storeOrder({
-            user_id: userId,
-            address_id: address_id,
-            total_amount: totalAmount,
-            delivery_cost: 800,
-            status: 'pending', 
-            payment_method: 'none',
-            payment_status: 'unpaid',
-            payment_reference: '',
-        });
-
-        cart.items.map(async (item, key) => {
-            await storeOrderItem({
-                order_id: order.id,
-                product_id: item?.productId,
-                quantity: item?.quantity || 0,
-                unit_price: item?.price || 0
-            })
-        })
-
-        const paystackRes = await axios.post(
-            "https://api.paystack.co/transaction/initialize",
-            {
-                email: req?.user?.email,
-                amount: totalAmount * 100,
-                callback_url: `${process.env.BASE_URL}/api/payment/verify`,
-                metadata: {
-                    orderId: order.id
-                }
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
-                }
-            }
-        );
-
-        await updateOrder(order.id, {
-            payment_reference: paystackRes.data.data.reference,
-        });
-
-        res.status(201).json({
-            paymentUrl: paystackRes.data.data.authorization_url,
-            orderId: order.id
-        });
-
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
+    if (!cart || cart.items.length === 0) {
+        return res.status(400).json({ message: "Cart is empty" });
     }
+
+    const totalAmount = cart?.total || 0
+    // const totalAmount = cart.items.reduce((acc, item) => {
+    //     return acc + item.product.price * item.quantity;
+    // }, 0);
+
+    const order = await storeOrder({
+        user_id: userId,
+        address_id: address_id,
+        total_amount: totalAmount,
+        delivery_cost: 800,
+        status: 'pending', 
+        payment_method: 'none',
+        payment_status: 'unpaid',
+        payment_reference: '',
+    });
+
+    cart.items.map(async (item, key) => {
+        await storeOrderItem({
+            order_id: order.id,
+            product_id: item?.productId,
+            quantity: item?.quantity || 0,
+            unit_price: item?.price || 0
+        })
+    })
+
+    const paystackRes = await axios.post(
+        "https://api.paystack.co/transaction/initialize",
+        {
+            email: req?.user?.email,
+            amount: totalAmount * 100,
+            callback_url: `${process.env.BASE_URL}/api/payment/verify`,
+            metadata: {
+                orderId: order.id
+            }
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
+            }
+        }
+    );
+
+    await updateOrder(order.id, {
+        payment_reference: paystackRes.data.data.reference,
+    });
+
+    res.status(201).json({
+        paymentUrl: paystackRes.data.data.authorization_url,
+        orderId: order.id
+    });
 }
 
 
 export const verifyPayment = async (req: express.Request, res: express.Response) => {
-    try {
-        const { reference } = req.query as { reference: string};
+    const { reference } = req.query as { reference: string};
 
-        const response = await axios.get(
-            `https://api.paystack.co/transaction/verify/${reference}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
-                }
+    const response = await axios.get(
+        `https://api.paystack.co/transaction/verify/${reference}`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
             }
-        );
-
-        const data = response.data.data;
-
-        if (data.status === "success") {
-            const order = await fetchOrderByPaymentReference(reference)
-            
-            if (!order) return res.status(404).json({ message: "Order not found" });
-
-            await updateOrder(order.id, {
-                payment_status: "paid",
-                status: "paid",
-            });
-
-            return res.json({ message: "Payment successful" });
         }
+    );
 
-        res.status(400).json({ message: "Payment failed" });
+    const data = response.data.data;
 
-    } catch (err: any) {
-        res.status(500).json({ error: err.message });
+    if (data.status === "success") {
+        const order = await fetchOrderByPaymentReference(reference)
+        
+        if (!order) return res.status(404).json({ message: "Order not found" });
+
+        await updateOrder(order.id, {
+            payment_status: "paid",
+            status: "paid",
+        });
+
+        return res.json({ message: "Payment successful" });
     }
+
+    res.status(400).json({ message: "Payment failed" });
 };
 
 
