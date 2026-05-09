640d760d255765321de71d8bf499fdd8b98795ae
Oxcode <mrexcelsam1@gmail.com>
2026-05-09 12:35:02 +0100

    modify

---
+++ src/helpers/response.ts
@@ -0,0 +1,22 @@
+// const currentDateTime = require('../lib/current.date.time');
+
+// const currentDateTime = () => new Date().toISOString();
+
+// exports.successResponse = (resultCode: string, title: string, message: string, data, maintenance) => ({
+//     result_code: resultCode,
+//     time: currentDateTime(),
+//     maintenance_info: maintenance || null,
+//     result: {
+//         title, message, data
+//     }
+// });
+
+
+// exports.errorResponse = (resultCode, title, error, maintenance) => ({
+//     result_code: resultCode,
+//     time: currentDateTime(),
+//     maintenance_info: maintenance || null,
+//     result: {
+//         title, error
+//     }
+// });
